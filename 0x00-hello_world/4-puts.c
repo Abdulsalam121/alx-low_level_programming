b8c8ab54522e474b077ea0650d6576fcf977b7ff
Abdulsalam <okunlolaabdulsalam@gmail.com>
2022-03-11 07:32:38 -0800

    advanced

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints phrase
  * void
- * return: 0 if properly exited
+ * Return: 0 if properly exited
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-/*main prints phrase with proper grammar but looks like an art piece*/
+/**
+ * main prints phrase with 
+ * proper grammar but looks 
+ * like an art piece
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
