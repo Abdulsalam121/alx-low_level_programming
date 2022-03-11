d03cd7c9521c83ddda2eafb795eddbee4aeef462
Abdulsalam <okunlolaabdulsalam@gmail.com>
2022-03-10 20:17:11 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,7 @@
  * main: prints phrase
  * return 0 if properly exited
  */
-int main(void)
-{
+int main(void){
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return 0;
 }
