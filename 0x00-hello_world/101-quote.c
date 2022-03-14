fd51ed064a5100ff93e7f98caede719a6ffe50a6
Abdulsalam <okunlolaabdulsalam@gmail.com>
2022-03-14 00:56:16 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 
 /**
  * main - prints piece of art is useful
- * 
- * Return: Always 0 (Success) 
+ *
+ * Return: Always (Success)
  */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
