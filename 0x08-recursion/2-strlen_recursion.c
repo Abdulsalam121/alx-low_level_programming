cea88fce2d53f0aec37f7a232b34507f2655d242
Abdulsalam <okunlolaabdulsalam@gmail.com>
2022-03-30 07:21:29 -0700

    Mandatory tasks

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,7 +12,7 @@ int _strlen_recursion(char *s)
 
 	if (*s > '\0')
 	{
-			n += _strlen_recursion(s + 1) + 1;
+		n += _strlen_recursion(s + 1) + 1;
 	}
 	return (n);
 }
--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,10 +12,10 @@ int _pow_recursion(int x, int y)
 {
 
 	if (y < 0)
-			return (-1);
+		return (-1);
 	else if (y == 1)
-			return (x);
+		return (x);
 	else if (y == 0)
-			return (1);
+		return (1);
 	return (x * _pow_recursion(x, y - 1));
 }
